aeedc665c6f005db0797d7151a4aab54e194c444
Olawuyi Abdulkareem <47379564+villarnovar@users.noreply.github.com>
2020-02-11 13:15:32 +0100

    Add files via upload

--- movies.js
+++ movies.js
@@ -17,26 +17,27 @@ button.addEventListener('click', function(e){
     .then(validateResponse)
 .then(function(response){
     return response.json();
-    alert("Movie not found")
+
 }).then(processData)
 });
 
 function processData(data){
-    //console.log(data);
-    htmlContent=`<div><img src="${data.Poster}" alt="${data.Title}"></div>
+    console.log(data);
+    htmlContent = `
+    <div><img src="${data.Poster}"  id="image" alt="${data.Title}"></div>
     <div><h2>${data.Title}</h2></div>
     <div><h3>ACTORS: ${data.Actors}</h3></div>
     <div><h3>DIRECTOR: ${data.Director}</h3></div>
+    <div><h3>WRITER: ${data.Writer}</h3></div>
     <div><h3>GENRE: ${data.Genre}</h3></div>
     <div><h3>PLOT: ${data.Plot}</h3></div>
     <div><h3>RELEASED DATE: ${data.Released}</h3></div>
     <div><h3>RUNTIME: ${data.Runtime}</h3></div>
     <div><h3>TYPE: ${data.Type}</h3></div>
-    <div><h3>RATINGS: ${data.Ratings[0].Value}</h3></div>
-
-    `;
+    <div><h3>BOX OFFICE: ${data.BoxOffice}</h3></div>
+    <div><h3>RATINGS: ${data.Ratings[0].Value}</h3></div>`;
     info.innerHTML = htmlContent;
-    
 }
+
 
 
